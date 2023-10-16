0445f304ab028162e23319d3e2d52f96d21e178c
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2023-10-16 11:52:12 -0700

    Adds gnome-terminal

---
+++ gnome-terminal/README.md
@@ -0,0 +1,3 @@
+# gnome-terminal theme
+I look much of this from github.com/dracula/gnome-terminal. See the LICENSE file for more info.
+
---
+++ gnome-terminal/install-ubuntu.sh
@@ -0,0 +1,37 @@
+apt-get install -y gnome-terminal
+
+mkdir -p $HOME/.dir_colors
+ln -s $PWD/dircolors $HOME/.dir_colors/dircolors
+
+dconfdir=/org/gnome/terminal/legacy/profiles:
+
+# make new profile
+profile_ids_old="$(dconf read "$dconfdir"/list | tr -d "]")"
+profile_id="$(uuidgen)"
+
+dconf write $dconfdir/list \
+    "${profile_ids_old}, '$profile_id']"
+dconf write "$dconfdir/:$profile_id"/visible-name "'Dracula'"
+
+
+to_dconf() {
+    tr '\n' '~' | sed -e "s#~\$#']\n#" -e "s#~#', '#g" -e "s#^#['#"
+}
+
+profile_dir="$dconfdir/:$profile_id"
+# set color palette
+dconf write $profile_dir/palette "$(to_dconf < $PWD/palette)"
+
+# set foreground, background and highlight color
+dconf write $profile_dir/bold-color "'$(cat $PWD/bd_color)'"
+dconf write $profile_dir/background-color "'$(cat $PWD/bg_color)'"
+dconf write $profile_dir/foreground-color "'$(cat $PWD/fg_color)'"
+
+# make sure the profile is set to not use theme colors
+dconf write $profile_dir/use-theme-colors "false"
+
+# set highlighted color to be different from foreground color
+dconf write $profile_dir/bold-color-same-as-fg "false"
+
+# set as default profile
+dconf write $dconfdir/default "'$profile_id'"
