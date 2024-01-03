6e9baeda7999526c510dc49a77c8573eccc66f0c
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2024-01-03 13:07:00 -0800

    waybaring

--- sway/install-ubuntu.sh
+++ sway/install-ubuntu.sh
@@ -76,6 +76,7 @@ rm -r mako
 mkdir -p $HOME/.config/mako
 ln -s $PWD/makoconfig $HOME/.config/mako/config
 
+# kanshi does automatic display switcheroo-ing
 git clone https://git.sr.ht/~emersion/kanshi
 cd kanshi
 meson build
@@ -86,5 +87,37 @@ rm -r kanshi
 
 mkdir -p $HOME/.config/kanshi
 
+# waybar is just a better status bar than is stock with sway
+sudo apt install \
+  clang-tidy \
+  gobject-introspection \
+  libdbusmenu-gtk3-dev \
+  libevdev-dev \
+  libfmt-dev \
+  libgirepository1.0-dev \
+  libgtk-3-dev \
+  libgtkmm-3.0-dev \
+  libinput-dev \
+  libjsoncpp-dev \
+  libmpdclient-dev \
+  libnl-3-dev \
+  libnl-genl-3-dev \
+  libpulse-dev \
+  libsigc++-2.0-dev \
+  libspdlog-dev \
+  libwayland-dev \
+  scdoc \
+  upower \
+  libxkbregistry-dev \
+  pavucontrol
+git clone https://github.com/Alexays/Waybar
+cd Waybar
+meson build
+ninja -C build
+ninja -C build install
+cd -
+rm -r Waybar
+
+ln -s $PWD/waybar $HOME/.config/waybar
 # misc scripts
 ln -s $PWD/open-music-workspace $HOME/.local/bin/open-music-workspace
