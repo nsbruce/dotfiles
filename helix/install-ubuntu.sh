1c452519ece8e6b4d69685c3dde437073ea081e5
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2025-03-21 13:58:15 -0700

    feat(helix): add more lsps

--- helix/install-ubuntu.sh
+++ helix/install-ubuntu.sh
@@ -13,3 +13,10 @@ ln -s $PWD/languages.toml $HOME/.config/helix/languages.toml
 cargo install --locked dprint
 mkdir -p $HOME/.config/dprint
 ln -s $PWD/dprint.json $HOME/.config/dprint/dprint.json
+
+# bash lsp and linter
+apt install -y --no-intsall-recommends shellcheck
+snap install bash-language-server --classic
+
+# markdown lsp
+cargo install --locked --git https://github.com/Feel-ix-343/markdown-oxide.git markdown-oxide
