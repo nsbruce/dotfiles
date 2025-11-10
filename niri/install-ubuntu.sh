ba4409a5c9f219241b787c80b744b2049924b74c
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2025-11-10 08:43:45 -0800

    feat(niri): improving install and keyboard shortcuts

--- niri/install-ubuntu.sh
+++ niri/install-ubuntu.sh
@@ -1,4 +1,13 @@
 apt-get install --assume-yes clang libudev-dev libgbm-dev libxkbcommon-dev libegl1-mesa-dev libwayland-dev libinput-dev libdbus-1-dev libsystemd-dev libseat-dev libpipewire-0.3-dev libpango1.0-dev libdisplay-info-dev
+
+# xwayland-satellite is a dep since I want xwayland support
+git clone https://github.com/Supreeeme/xwayland-satellite.git
+cd xwayland-satellite
+cargo install --path .
+cd -
+rm -rf xwayland-satellite
+
+# niri itself
 git clone https://github.com/YaLTeR/niri.git
 cd niri
 cargo install --path . --locked
