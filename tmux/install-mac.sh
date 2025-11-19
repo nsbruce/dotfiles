ea59a2ca55e3569f6913f244375ad26b5d7784ee
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2025-11-19 09:23:54 -0800

    feat(tmux): remove tmux config
    I now use zellij
    
    BREAKING CHANGE:

--- tmux/install-mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-brew install git tmux reattach-to-user-namespace
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-python3 -m pip install --user libtmux
-
-ln -s $PWD/tmux.conf $HOME/.tmux.conf
--- tmux/install-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-apt-get install -y xsel python3-pip tmux
-
-python3 -m pip install --user libtmux
-
-ln -s $PWD/tmux.conf $HOME/.tmux.conf
