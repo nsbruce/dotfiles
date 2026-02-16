f8441c5550bf84a295ba84cc4db3659921e2d39b
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2026-02-16 12:49:22 -0800

    feat(all): tweaks

--- kmonad/kmonad.zsh
+++ kmonad/kmonad.zsh
@@ -4,6 +4,8 @@ get_best_kbd() {
     # get the best keyboard for the current setup
     if [[ -e "/dev/input/by-id/usb-Chicony_HP_Elite_USB_Keyboard-event-kbd" ]]; then
         echo "CURRENT_KBD=/opt/kmonad/hp-elite-chicony.kbd" > /opt/kmonad/current_kbd
+    elif [[ -e "/dev/input/by-id/usb-CHICONY_HP_Basic_USB_Keyboard-event-kbd" ]]; then
+        echo "CURRENT_KBD=/opt/kmonad/hp-basic-chicony.kbd" > /opt/kmonad/current_kbd
     elif [[ -e "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ]]; then
         echo "CURRENT_KBD=/opt/kmonad/hp-elitebook.kbd" > /opt/kmonad/current_kbd
     else
