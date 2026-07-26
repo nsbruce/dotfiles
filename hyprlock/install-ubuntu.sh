e01e2633bab7bfde456a8ff912e78c6ebb4d0161
Nicholas Bruce <nicholas.bruce@nrc-cnrc.gc.ca>
2026-07-26 16:30:01 -0700

    feat(hyprlock/niri/waybar): improve installs

--- hyprlock/install-ubuntu.sh
+++ hyprlock/install-ubuntu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-add-apt-repository ppa:cppiber/hyprland
+add-apt-repository --yes --ppa ppa:cppiber/hyprland
 apt-get update
 apt install --assume-yes hyprlock
 
-ln -s $PWD/hyprlock.conf $HOME/.config/hypr/hyprlock.conf
+mkdir --parents "$HOME/.config/hypr"
+ln -s "$PWD/hyprlock.conf" "$HOME/.config/hypr/hyprlock.conf"
--- waybar/install-ubuntu.sh
+++ waybar/install-ubuntu.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # waybar is just a better status bar than is stock with sway
-apt install --assume-yes waybar
+apt install --assume-yes waybar pavucontrol
 
-mkdir --parents $HOME/.config/waybar
-ln -s $PWD/colors.css $PWD/style.css $PWD/config $HOME/.config/waybar/
-cp $PWD/get-vpn-state.zsh $HOME/.config/waybar/
+mkdir --parents "$HOME/.config/waybar"
+ln -s "$PWD/colors.css" "$PWD/style.css" "$PWD/config" "$HOME/.config/waybar/"
+cp "$PWD/get-vpn-state.zsh" "$HOME/.config/waybar/"
+cp "$PWD/toggle-vpn.zsh" "$HOME/.config/waybar/"
+
+systemctl --user start waybar.service
